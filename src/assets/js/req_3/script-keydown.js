5e51e444250b65d1bb278b2c94a1dfaeb12be7bd
Jona <jonathan.diaz@driv.in>
2024-06-22 15:54:35 -0400

    add compatibility lisener

--- src/assets/js/req_3/script-keydown.js
+++ src/assets/js/req_3/script-keydown.js
@@ -33,3 +33,35 @@ inputColorChange.addEventListener('keydown', event => {
       break;
   }
 });
+
+// For compatibility on mobile devices
+inputColorChange.addEventListener('input', event => {
+  switch (event.data) {
+    case 'a':
+      color = 'pink'
+      keyContainer.style.backgroundColor = color;
+      break;
+    case 's':
+      color = 'orange'
+      keyContainer.style.backgroundColor = color;
+      break;
+    case 'd':
+      color = 'lightblue'
+      keyContainer.style.backgroundColor = color;
+      break;
+    case 'q':
+      color = 'purple'
+      keyContainer2.style.backgroundColor = color;
+      break;
+    case 'w':
+      color = 'gray'
+      keyContainer2.style.backgroundColor = color;
+      break;
+    case 'e':
+      color = 'brown'
+      keyContainer2.style.backgroundColor = color;
+      break;
+    default:
+      break;
+  }
+});
